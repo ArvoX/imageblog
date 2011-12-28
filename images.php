d7c6b4e899d3eb5f63c5482fe595afab8357ab77
ArvoX <github@arvox.dk>
2011-12-29 00:00:15 +0100

    Image display
    Made an image script to get the image from the database and set it up widt nice
     ulrs.

---
+++ images.php
@@ -0,0 +1,27 @@
+<?php
+$name = $_GET['img'];
+$data = Data::getImage($name);
+
+function ext_to_mime_type($ext)
+{
+	switch($ext)
+	{
+		case 'jpg':
+			$ext = 'jpeg';
+		case 'jpeg':
+		case 'tiff':
+		case 'bmp':
+		case 'psd':
+		case 'png':
+		case 'gif':
+			return "image/$ext";
+	}
+}
+
+header('Content-type: '.ext_to_mime_type($data->imageType));
+header('Content-Length: '.strlen($data->image));
+header('Last-Modified: '.date('r', $data->updated));
+
+echo $data->image;
+
+?>
